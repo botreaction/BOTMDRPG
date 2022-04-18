c4fc3ec0e42125b384d15a516f38e1724a18d37e
botreaction <waduhbro321@gmail.com>
2022-04-18 09:44:14 +0700

    MUKABAJINGAN

---
+++ settings.js
@@ -0,0 +1,72 @@
+/*
+   * Create By Dika Ardnt.
+   * Contact Me on wa.me/6288292024190
+   * Follow https://github.com/DikaArdnt
+*/
+
+const fs = require('fs')
+const chalk = require('chalk')
+
+// Website Api
+global.APIs = {
+	zenz: 'https://zenzapi.xyz',
+}
+
+// Apikey Website Api
+global.APIKeys = {
+	'https://zenzapi.xyz': '4AD2E985D2', // Kalau habis beli sendiri
+}
+
+// Other
+global.owner = ['6289688206739']
+global.premium = ['6289688206739']
+global.ownernomer = ['6289688206739']
+global.ownername = 'W I L Y - B O T Z'
+global.botname = 'H E N T O N G E R S - B O T Z'
+global.footer = 'W I L Y - B O T Z'
+global.ig = 'https://chat.whatsapp.com/CAPptYDgkOI3V4vAOcC3tW/e'
+global.email = 'https://chat.whatsapp.com/CAPptYDgkOI3V4vAOcC3tW/'
+global.region = 'Indonesia'
+global.sc = 'https://chat.whatsapp.com/CAPptYDgkOI3V4vAOcC3tW/'
+global.myweb = 'https://chat.whatsapp.com/CAPptYDgkOI3V4vAOcC3tW/'
+global.packname = 'H E N T O N G E R S - B O T Z'
+global.author = 'H E N T O N G E R S - B O T Z'
+global.sessionName = 'chika'
+global.prefa = ['','!','.','🐦','🐤','🗿']
+global.sp = '⭔'
+global.mess = {
+    success: '```✅ Success Bro………```',
+    admin: 'Fitur Khusus Admin Group!',
+    botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!',
+    owner: 'Fitur Khusus Owner Bot',
+    group: 'Fitur Digunakan Hanya Untuk Group!',
+    private: 'Fitur Digunakan Hanya Untuk Private Chat!',
+    bot: 'Fitur Khusus Pengguna Nomor Bot',
+    wait: '```L O A D I N G……...```',
+    endLimit: 'Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12',
+}
+    global.limitawal = {
+    premium: "Infinity",
+    free: 2,
+    monayawal: 1000
+}
+   global.rpg = {
+   darahawal: 100,
+   besiawal: 15,
+   goldawal: 10,
+   emeraldawal: 5,
+   umpanawal: 5,
+   potionawal: 1
+}
+global.thumb = fs.readFileSync('./image/chika.jpg')
+global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
